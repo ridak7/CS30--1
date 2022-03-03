77f54df6523a291afdf1ae8f8f83f6b7ea0a070f
904273001 <904273001@L815WD150060.edu.cbe.ab.ca>
2022-03-03 09:18:24 -0700

    g

--- All Backed.zip_expanded/Tutorial_File/src/Sales_Center_Walwart_Example_Template/Walwart_Centre.java
+++ All Backed.zip_expanded/Tutorial_File/src/Sales_Center_Walwart_Example_Template/Walwart_Centre.java
@@ -1,6 +1,13 @@
 package Sales_Center_Walwart_Example_Template;
 
-public class Walwart_Centre {
+import java.text.NumberFormat;
 
+public class Walwart_Centre 
+
+{
+
+	NumberFormat money = NumberFormat.getCurrencyInstance();
+	
+	
 }
    
