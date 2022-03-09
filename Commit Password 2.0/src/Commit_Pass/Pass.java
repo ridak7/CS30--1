d57d1c8c852a27117cd514fc68f830c7beee8693
904273001 <904273001@L815WD150060.edu.cbe.ab.ca>
2022-03-09 11:01:04 -0700

    Pass

---
+++ Commit Password 2.0/src/Commit_Pass/Pass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Commit_Pass;
+
+/**
+ * @author 904273001
+ *
+ */
+public class Pass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("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");
+	}
+
+}
